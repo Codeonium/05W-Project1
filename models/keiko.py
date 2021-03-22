27fe83eda48fea14624f0cab0a64c378f5b7c633
codeonium <codeon2020@gmail.com>
2021-03-22 14:45:31 +0000

    keiko repository prepared, still need to look at a function to limit the keikos space

--- models/keiko.py
+++ models/keiko.py
@@ -1,8 +1,8 @@
 class Keiko:
 
-    def __init__(self, sensei, time, space, attendance, id = None):
+    def __init__(self, sensei, time, space, deshi, id = None):
         self.sensei = sensei
         self.time = time
         self.space = space
-        self.attendance = attendance
+        self.deshi = deshi
         self.id = id
---
+++ repositories/keiko_repository.py
@@ -0,0 +1,53 @@
+from db.run_sql import run_sql
+
+from models.deshi import Deshi
+from models.keiko import Keiko
+from models.sensei import Sensei
+import repositories.deshi_repository as deshi_repository
+import repositories.sensei_repository as sensei_repository
+
+def save (keiko):
+    sql = "INSERT INTO keikos ( sensei_id, time, space, deshi_id) VALUES (%s, %s, %s, %s) RETURNING id"
+    values = [keiko.sensei.id, keiko.time, keiko.space, keiko.deshi.id]
+    results = run_sql(sql, values)
+    id = results[0]['id']
+    keiko.id = id
+
+
+def select_all():
+    keikos = []
+    sql = "SELECT * FROM keikos"
+    results = run_sql(sql)
+    for result in results:
+        deshi = deshi_repository.select(result["deshi_id"])
+        sensei = sensei_repository.select(result["sensei_id"])
+        keiko = Keiko(sensei, result["time"], result["space"], deshi, result["id"])
+        keikos.append(keiko)
+    return keikos
+
+
+def select(id):
+    sql = "SELECT * FROM keikos WHERE id = %s"
+    values = [id]
+    result = run_sql(sql, values)[0]
+    deshi = deshi_repository.select(result["deshi_id"])
+    sensei = sensei_repository.select(result["sensei_id"])
+    keiko = Keiko(sensei, result["time"], result["space"], deshi, result["id"])
+    return keiko
+
+
+def delete_all():
+    sql = "DELETE FROM keikos"
+    run_sql(sql)
+
+
+def delete(id):
+    sql = "DELETE FROM keikos WHERE id = %s"
+    values = [id]
+    run_sql(sql, values)
+
+
+def update(keiko):
+    sql = "UPDATE keikos SET (sensei_id, time, space, deshi_id) = (%s, %s, %s, %s) WHERE id = %s"
+    values = [keiko.sensei.id, keiko.time, keiko.space, keiko.deshi.id, keiko.id]
+    run_sql(sql, values)
