de15f1f63dcbf9c6093d5e8437322cb4f07d184f
codeonium <codeon2020@gmail.com>
2021-03-22 10:59:00 +0000

    db dojos sql started to populate

--- db/dojos.sql
+++ db/dojos.sql
@@ -1,11 +1,26 @@
-DROP TABLE IF EXISTS 
-DROP TABLE IF EXISTS
+DROP TABLE IF EXISTS keikos;
+DROP TABLE IF EXISTS senseis;
+DROP TABLE IF EXISTS dechis;
 
-
-CREATE TABLE (
+CREATE TABLE dechis (
     id SERIAL PRIMARY KEY,
+    name VARCHAR (255),
+    level INT,
+    -- knowledge VARCHAR(255) i Want this to be a list must look for how
 )
 
-CREATE TABLE (
+CREATE TABLE senseis (
     id SERIAL PRIMARY KEY,
+    name VARCHAR (255),
+    level INT,
+    -- knowledge VARCHAR(255) i Want this to be a list must look for how
+)
+
+CREATE TABLE keikos (
+    id SERIAL PRIMARY KEY,
+    sensei_id INT REFERENCES senseis(id) ON DELETE CASCADE,
+    -- time // i want this to display a 24hours time
+    space INT,
+    level INT REFERENCES senseis(level) ON DELETE CASCADE,
+    attendance INT REFERENCES deshis(id) ON DELETE CASCADE
 )
--- models/keiko.py
+++ models/keiko.py
@@ -1,8 +1,9 @@
 class Keiko:
 
-    def __init__(self, sensei, time, space, level, id = None):
+    def __init__(self, sensei, time, space, level, attendance, id = None):
         self.sensei = sensei
         self.time = time
         self.space = space
         self.level = level
+        self.attendance = attendance
         self.id = id
