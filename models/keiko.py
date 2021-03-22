e161176c2e9dbfa10ca12b33072cba118fde179d
codeonium <codeon2020@gmail.com>
2021-03-22 10:46:01 +0000

    models created, as a base for guidance

---
+++ models/keiko.py
@@ -0,0 +1,8 @@
+class Keiko:
+
+    def __init__(self, sensei, time, space, level, id = None):
+        self.sensei = sensei
+        self.time = time
+        self.space = space
+        self.level = level
+        self.id = id
---
+++ models/sensei.py
@@ -0,0 +1,7 @@
+class Sensei:
+
+    def __init__(self, name, level, knowledge, id = None ):
+        self.name = name
+        self.level = level
+        self.knowledge = knowledge
+        self.id = id
